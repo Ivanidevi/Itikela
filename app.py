6cc57db012c162b59a868ce0ced14e53cd391405
Ivanidevi <vani36022@gmail.com>
2025-10-27 18:54:21 +0530

    ssh method

--- app.py
+++ app.py
@@ -1,14 +1,5 @@
-If your database is growing rapidly, which AWS service would you use for automatic scaling?
-
-How do you secure your S3 bucket so only specific users can access it?
-
-Your application needs low latency and global delivery. Which AWS service will help?
-
-You have sensitive data. How will you encrypt it in AWS?
-
-How would you connect your on-premises data center to AWS securely?
-
-What steps will you take if your EC2 instance stops unexpectedly?
-
-Your company wants to reduce AWS costs. What services or techniques will you suggest?
-
+resource "aws_instance" "local"
+type: t2.micro
+ami: 1819819832838929
+vpc : 12345678
+subnet : 56789
